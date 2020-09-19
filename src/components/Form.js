0a140dda0a3bcd809848892b0729b696b3d44278
keeks5456 <Akikogreen08@gmail.com>
2020-09-19 16:22:09 -0700

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+// import DisplayData from './DisplayData'
+
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: []
   }
+  // We don't have a server to send our data to, but to demonstrate submission, we could modify our Form component to list out submissions, storing them in state:
 
   handleFirstNameChange = event => {
+    
     this.setState({
       firstName: event.target.value
     })
@@ -18,12 +23,34 @@ class Form extends React.Component {
     })
   }
 
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    // debugger
+
+    let formData = {firstName: this.state.firstName, lastName: this.state.lastName}
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray}) 
+  }
+
+  listOfSubmissions = () =>{
+    
+    return this.state.submittedData.map(data =>{
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div> 
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
-      <form>
+      <div>
+      <form onSubmit={(e) => this.handleSubmit(e)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <input type="submit" value="Submit" />
       </form>
+      {this.listOfSubmissions()}
+      </div>
+
     )
   }
 }
